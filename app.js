4013b39648a5fc7cf6d251e7b874ef71a3e08d1a
Mayur Thosar <mayurthosar.mt@gmail.com>
2017-08-15 18:57:08 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const {app,BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+var APP_DIR = '/app/';
+
+function createWindow(){
+    win = new BrowserWindow({
+        width:1000,
+        height:600,
+        frame:false
+    });
+
+    // win.openDevTools({detach:true})
+
+    win.loadURL(url.format({
+        pathname:path.join(__dirname,APP_DIR,'index.html'),
+        protocol:'file:',
+        slashes:true
+    }));
+
+}
+app.on('ready',createWindow);
+app.on('window-all-closd', () => {
+
+    if(process.platform !== 'darwin'){
+        app.quit();
+}
+});
+
